ba86731572d800706c4093b6434c00f59ba736c0
syedfaizz <syedfaizz45@gmail.com>
2021-07-31 17:46:54 +0530

    [faisal] Add: welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
